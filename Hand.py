3cbf592a92a7d68e9e2b2e965d30d59c6c153765
ZetsubouNate <mirelacalinescu2000@yahoo.com>
2023-02-13 02:29:24 +0200

    plec la magazin

--- Hand.py
+++ Hand.py
@@ -14,3 +14,11 @@ class Hand:
 
     def add_card(self, card):
         self.value += card.value
+        self.cards.append(card)
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def ace_adjust(self):
+        while self.aces and not self.value <= 21:
+            self.aces -= 1
+            self.value -= 10
